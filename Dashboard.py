d546e5a370e0aadbcdb03305cd5748aa1c00c4d2
Abror Muhammad Hazim <83820580+zim18@users.noreply.github.com>
2024-06-03 14:46:13 +0700

    Update Dashboard.py

--- Dashboard.py
+++ Dashboard.py
@@ -5,7 +5,7 @@ import cv2
 import numpy as np
 
 root_dir=os.path.dirname(os.path.abspath(__file__))
-homepage_img_dir=os.path.join(root_dir,"assets","images")
+homepage_img_dir=os.path.join(root_dir,"Assets","Images")
 st.title("Klasifikasi Kepadatan Lalu Lintas dengan Model Convolutional Neural Network Menggunakan Arsitektur EfficientNet ")
 st.subheader(""" Kelompok 1""")    
 st.markdown("""
